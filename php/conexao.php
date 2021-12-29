bdb46cbd29359a9d38353b72f457a5c1ab78065c
Márcio Fernando Maia <marciofernando.maia@yahoo.com.br>
2021-12-29 10:32:47 -0300

    Added archives in some paths

---
+++ php/conexao.php
@@ -0,0 +1,14 @@
+<?php
+
+$servidor = "localhost";
+$usuario = "root";
+$senha = " ";
+$banco = "cadastro";
+
+
+//Create the connection
+$conn = new mysqli($servidor, $usuario, $senha, $banco);
+	
+
+
+?>
---
+++ php/salva-mensagem.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once ('php/conexao.php');
+
+// Receive the data  
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+$sexo = $_POST['sexo'];
+$peso = $_POST['peso'];
+$altura = $_POST['altura'];
+$nacionalidade = $_POST['nacionalidade'];
+
+
+$sql = "INSERT INTO cadastro (nome, idade, sexo, peso, altura, nacionalidade) 
+VALUES ('$nome','$idade','$sexo','$peso','$altura'$nacionalidade)";
+
+
+if ($conn->query($sql) === TRUE) {
+    echo "Novo registro criado com sucesso!";
+  } else {
+    echo "Erro: " . $sql . "<br>" . $conn->error;
+  }
+  
+  $conn->close();
+
+
+
+?>
